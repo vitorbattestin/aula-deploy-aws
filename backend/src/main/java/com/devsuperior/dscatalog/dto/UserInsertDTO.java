80e67d15198cf8e11a788617622002c2bd1cb164
Vitor Battestin <vitor.battestin@gmail.com>
2021-07-27 16:26:31 -0300

    First commit

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
+public class UserInsertDTO extends UserDTO{
+
+    private String password;
+
+    public UserInsertDTO(){
+        super();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
